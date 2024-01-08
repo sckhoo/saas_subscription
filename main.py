158dc2420f02c7246be6592e3c9beb662da50a94
SC Khoo <sckhoo@gmail.com>
2024-01-08 18:33:31 +0800

    added product and classmethod

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from merchant_schema import Merchant
+from product_schema import Product
 from datetime import datetime
 
 
@@ -9,9 +10,20 @@ new_marchant = Merchant(
     merchant_detail='sourdough',
     merchant_date=datetime.today(),
     merchant_url='khooville.com',
-    merchant_socmed=['x/sckhoo', 'ig/sckhoo', 'fb/sckhoo']
+    merchant_socmed='sourdough',
 )
+
+
 
 print(new_marchant.merchant_socmed)
 
 
+# new_marchant = Merchant(
+#     merchant_id="abcd1234",
+#     merchant_email='sckhoo@gmail.com',
+#     merchant_name='sckhoo',
+#     merchant_detail='sourdough',
+#     merchant_date=datetime.today(),
+#     merchant_url='khooville.com',
+#     merchant_socmed=['twitter.com/sckhoo', 'instagram/sckhoo', 'facebook.com/sckhoo']
+# )
--- merchant_schema.py
+++ merchant_schema.py
@@ -10,10 +10,3 @@ class Merchant(BaseModel):
     merchant_url : Optional[str]
     merchant_socmed : Optional[list]
     merchant_date : datetime
-    
-    @validator("merchant_id")
-    def validate_merchant_id(cls, merchant_id: str):
-        if (len(merchant_id) == 8) and (merchant_id.isalnum()):
-            return merchant_id
-        else:
-            print("merchant_id not right format")
