a906f586e167c61e127788ac6c66a9e23591eefd
sckhoo <sckhoo@gmail.com>
2024-01-18 11:48:03 +0800

    MBP

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from merchant_schema import Merchant
 from product_schema import Product
 from datetime import datetime
+import json
 
 
 new_marchant = Merchant(
@@ -10,26 +11,9 @@ new_marchant = Merchant(
     merchant_detail='sourdough',
     merchant_date=datetime.today(),
     merchant_url='khooville.com',
-    merchant_socmed=['x/sckhoo', 'fb/sckhoo'],
-)
-
-new_product = Product(
-    product_id="abcd1234",
-    product_name='sckhoo',
-    product_detail='sourdough',
-    product_date=datetime.today(),
+    merchant_socmed=['x/sckhoo', 'ig/sckhoo', 'fb/sckhoo']
 )
 
 print(new_marchant.merchant_socmed)
-print(new_marchant.add(new_marchant.dict()))
-print(new_marchant.search_by_id("khoo"))
-print(new_marchant.modify_by_id(new_marchant.dict()))
-print(new_marchant.suspend_by_id("id_to_be_suspended"))
-print(new_marchant.delete_by_id("id_to_be_deleted"))
 
-print(new_product.product_name)
-print(new_product.add(new_product.dict()))
-print(new_product.search_by_id("khoo"))
-print(new_product.modify_by_id(new_product.dict()))
-print(new_product.suspend_by_id("id_to_be_suspended"))
-print(new_product.delete_by_id("id_to_be_deleted"))
+
--- merchant_schema.py
+++ merchant_schema.py
@@ -8,7 +8,7 @@ class Merchant(BaseModel):
     merchant_detail : str
     merchant_email : EmailStr
     merchant_url : Optional[str]
-    merchant_socmed : Optional[list]
+    merchant_socmed : Optional[dict]
     merchant_date : datetime
 
     @classmethod
@@ -17,26 +17,15 @@ class Merchant(BaseModel):
         # return new object
         return f"add received {merchant_record}"
     
+    @validator("merchant_id")
+    def validate_merchant_id(cls, merchant_id: str):
+        if (len(merchant_id) == 8) and (merchant_id.isalnum()):
+            return merchant_id
+        else:
+            print("merchant_id not right format")
+            
     @classmethod
-    def search_by_id(cls, merchant_id):
+    def merchant_add(cls, new_merchant):
         # calculate age an set it as a age
         # return new object
-        return f"search_by_id received {merchant_id}"
-    
-    @classmethod
-    def modify_by_id(cls, merchant_to_modify):
-        # calculate age an set it as a age
-        # return new object
-        return f"modify_by_id received {merchant_to_modify}"
-    
-    @classmethod
-    def suspend_by_id(cls, merchant_id):
-        # calculate age an set it as a age
-        # return new object
-        return f"suspend_by_id received {merchant_id}"
-    
-    @classmethod
-    def delete_by_id(cls, merchant_id):
-        # calculate age an set it as a age
-        # return new object
-        return f"delete_by_id received {merchant_id}"
+        return cls(name, date.today().year - birth_year)
